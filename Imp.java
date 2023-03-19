3ff2e1b7e824d6b7999151f6ebcfdfe24187ab5d
An. Patil <123909570+mimzowich@users.noreply.github.com>
2023-03-19 16:41:15 -0700

    Update Imp.java

--- Imp.java
+++ Imp.java
@@ -1,13 +1,25 @@
+package Monsters;
+
+import Abilities.MeleeAttack;
+import Monsters.Monster;
+
 import java.util.HashMap;
 //Name:Anshu Patil
-public class Imp extends Monster{
+public class Imp extends Monster {
 
     public Imp(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
         super(maxHP, xp, items);
+        Integer maxStr = 15;
+        Integer maxDef = 6;
+        Integer maxAgi = 3;
+        attack = new MeleeAttack(this);
+        str = super.getAttribute(str, maxStr);
+        def = super.getAttribute(def,maxDef);
+        agi = super.getAttribute(agi,maxAgi);
     }
 
     @Override
     public String toString() {
-        return "Imp has" +  super.toString();
+        return "Monsters.Imp has" +  super.toString();
     }
 }
