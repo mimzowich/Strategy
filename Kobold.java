0994a8c5614bba258239b11b7217141a87ad8220
An. Patil <123909570+mimzowich@users.noreply.github.com>
2023-03-19 16:42:07 -0700

    Update Kobold.java

--- Kobold.java
+++ Kobold.java
@@ -1,14 +1,27 @@
+package Monsters;
+
+import Abilities.MeleeAttack;
+import Abilities.RangedAttack;
+import Monsters.Monster;
+
 import java.util.HashMap;
 //Name:Anshu Patil
-public class Kobold extends Monster{
+public class Kobold extends Monster {
 
 
     public Kobold(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
         super(maxHP, xp, items);
+        Integer maxStr = 15;
+        Integer maxDef = 6;
+        Integer maxAgi = 3;
+        attack = new RangedAttack(this);
+        str = super.getAttribute(str, maxStr);
+        def = super.getAttribute(def,maxDef);
+        agi = super.getAttribute(agi,maxAgi);
     }
 
     @Override
     public String toString() {
-        return "Kobold has" +  super.toString();
+        return "Monsters.Kobold has" +  super.toString();
     }
 }
