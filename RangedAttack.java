b12d9b43805dc319c8a08249f496b829f1402b8c
An. Patil <123909570+mimzowich@users.noreply.github.com>
2023-03-19 16:42:38 -0700

    Update RangedAttack.java

--- RangedAttack.java
+++ RangedAttack.java
@@ -1,13 +1,17 @@
+package Abilities;
+
+import Monsters.Monster;
+
 //Name:Anshu Patil
-public class RangedAttack implements Attack{
+public class RangedAttack implements Attack {
     Monster attacker;
     public RangedAttack(Monster attacker){
         this.attacker = attacker;
     }
     @Override
     public Integer attack(Monster target){
-        String message = attacker + " uses a ranged melee attack on " + target;
+        String message = attacker + " uses a ranged attack on " + target;
         System.out.println(message);
-        return null;
+        return attacker.getAgi() - target.getAgi();
     }
 }
