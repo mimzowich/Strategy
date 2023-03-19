ca6067ccf28f9cebfe2132fbebf20713f7ee88ef
An. Patil <123909570+mimzowich@users.noreply.github.com>
2023-03-19 16:40:41 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,3 +1,7 @@
+import Monsters.Imp;
+import Monsters.Kobold;
+import Monsters.Monster;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +15,10 @@ public class Driver {
         monsters.add(new Kobold(1,5,items));
         for(Monster m : monsters){
             System.out.println(m);
+        }
+        while((monsters.get(0).getHp()>0 && (monsters.get(1).getHp()>0))){
+            System.out.println(monsters.get(0).attackTarget(monsters.get(1)));
+            System.out.println(monsters.get(1).attackTarget(monsters.get(0)));
         }
 
 
