9ce77105987836b5fcba7dcc04bd1208cac1873b
An. Patil <123909570+mimzowich@users.noreply.github.com>
2023-03-19 16:39:03 -0700

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,13 +1,71 @@
+package Monsters;
+
+import Abilities.Attack;
+
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.Random;
 //Name:Anshu Patil
 
 public abstract class Monster {
     // instance variables
-    private int hp;
-    private int xp;
-    private int maxHP;
+    private Integer hp;
+    private Integer xp;
+
+    Integer agi = 10;
+    Integer def = 10;
+    Integer str = 10;
+    Attack attack;
+
+
+    public Attack getAttack() {
+        return attack;
+    }
+    boolean takeDamage(Integer damage) {
+        if (damage > 0) {
+            hp -= damage;
+            System.out.println("The creature was hit for " + damage + " damage.");
+
+            if (hp <= 0) {
+                System.out.println("Oh no! The creature has perished.");
+                hp = 0;
+            }
+
+            System.out.println(toString());
+            return hp > 0;
+        }
+        return true;
+    }
+
+    public Integer attackTarget(Monster target) {
+        int damage = attack.attack(target);
+
+        if (this.getHp() <= 0) {
+            System.out.println(target.toString());
+            return 0;
+        }
+
+        target.takeDamage(damage);
+        return damage;
+    }
+
+
+
+
+    Integer getAttribute(Integer min,Integer max){
+        Random rand = new Random();
+        if(min>max){
+            Integer temp = min;
+            min = max;
+            max = temp;
+        }
+        //returns a random number between min and max inclusive
+        return rand.nextInt(max-min) + min;
+    }
+
+    private Integer maxHP;
     private HashMap<String, Integer> items;
+
     public Monster(Integer maxHP, Integer xp, HashMap<String,Integer> items) {
         this.maxHP = maxHP;
         hp = this.maxHP;
@@ -35,6 +93,18 @@ public abstract class Monster {
     }
     public Integer getMaxHP() {
         return maxHP;
+    }
+
+    public Integer getAgi() {
+        return agi;
+    }
+
+    public Integer getDef() {
+        return def;
+    }
+
+    public Integer getStr() {
+        return str;
     }
 
     @Override
